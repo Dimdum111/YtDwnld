7b580b42b6e967519624f3c9b324bc92f284882d
Dimdum <tanomi364364@gmail.com>
2026-09-13 02:12:01 +0300

    feat: added views fetch, added some ui, added creating folder where to download

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from pytubefix import YouTube
 import tkinter as tk
 import os
 
-title = "Hi"
-author = ""
-views = ""
+os.makedirs("YtDwnld Downloads", exist_ok=True)
 
 def download():
     url = url_box.get()
     yt = YouTube(url)
-    title = yt.title
+    title, author = yt.title, yt.author
+    vid_title.config(text=title)
+    vid_author.config(text=f"By: {author}")
     print(f"Downloading: {title}")
 
 
@@ -21,14 +21,22 @@ root.geometry("400x300")
 # ------------------------
 
 # -- Creating UI --
-prog_name = tk.Label(root, text="YtDwnld", font=("Arial", 16))
+prog_name = tk.Label(root, text="YtDwnld", font=("Arial", 20))
 url_box = tk.Entry(root, width=50)
-vid_title = tk.Label(root, text=title, font=("Arial", 16))
-download_button = tk.Button(root, text="Download!", font=("Arial", 16), command=download)
+
+vid_title = tk.Label(root, text="", font=("Arial", 12))
+vid_author = tk.Label(root, text="", font=("Arial", 12))
+
+download_button = tk.Button(root, text="Download!", font=("Arial", 16), command=download, fg="green")
+# is_mp3 = tk.Checkbutton(root, text="Download as audio? (mp3)")
+
 prog_name.pack()
 vid_title.pack()
-url_box.pack(pady=84)
-download_button.pack()
+vid_author.pack()
+
+url_box.pack(pady=16)
+download_button.pack(pady=50)
+# is_mp3.pack()
 # -----------------
 
 root.mainloop()
